f61fc41178effb5223207c251ddfa4ddce9803ec
Abdul Raheem <abdulraheem.arhex@gmail.com>
2024-06-20 03:19:57 +0200

    JWT Fixes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
-const { secretKey } = process.env.JWT_SECRET;
+const secretKey = process.env.JWT_SECRET;
 
 const auth = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization.split(' ')[1];
 
   if (!token) {
     return res
@@ -15,6 +15,10 @@ const auth = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (error) {
+    console.log(error);
+
+
+
     return res.status(401).json({ message: "Unauthorized" });
   }
 };
